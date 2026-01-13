8530451831b03cb191cb47c696ca0d41ad6dd721
jahnavig03 <jahnavigundakaram1407@gmail.com>
2026-01-13 23:25:32 +0530

    Count-Primes

---
+++ Count Primes/Count-Primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;  
+
+        vector<bool> notPrime(n, false);
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+};
